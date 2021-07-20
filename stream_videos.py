a0b42ba0cebaccfb2234be559953a19c43457891
cedarwarman <cedardalewarman@gmail.com>
2021-07-19 22:51:59 -0700

    Working version

--- stream_videos.py
+++ stream_videos.py
@@ -1,5 +1,7 @@
 #!/usr/env/bin python3
 
+# Plays youtube videos based on links in Google spreadsheet
+
 # Adapted from:
 # https://stackoverflow.com/questions/67010182/python-script-wont-open-youtube-stream-in-vlc
 
@@ -14,7 +16,8 @@ best = video.getbest()
 i = vlc.Instance()
 media = vlc.MediaPlayer(best.url)
 
-media.toggle_fullscreen()
+#media.toggle_fullscreen()
+#media.set_fullscreen(True)
 
 if sys.platform == "darwin":
     from PyQt6 import QtCore
@@ -23,12 +26,15 @@ if sys.platform == "darwin":
     
     print("building app")
 
-    vlcApp =QtWidgets.QApplication(sys.argv)
+    vlcApp = QtWidgets.QApplication(sys.argv)
     vlcWidget = QtWidgets.QFrame()
-    vlcWidget.resize(700, 700)
     vlcWidget.showFullScreen()
+    #vlcWidget.showMaximized()
+
     media.set_nsobject(int(vlcWidget.winId()))
     print("Playing media")
+    #media.set_fullscreen(True)
     media.play()
     vlcApp.exec()
+    #media.set_fullscreen(True)
 
